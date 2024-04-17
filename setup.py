eccd0cf10e7f4851ab3c97675c07e7f8e9f3f376
Zander Lewis <wolfthedev@gmail.com>
2024-04-17 13:39:19 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sitepy',
+    version='0.0.1',
+    description='A simple web framework.',
+    author='WolfTheDev',
+    author_email='wolfthedev@gmail.com',
+    url='https://github.com/WolfTheDeveloper/sitepy',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+)
---
+++ sitepy/sitepy.py
@@ -0,0 +1,82 @@
+import os
+from urllib.parse import parse_qs
+from wsgiref.simple_server import make_server
+import inspect
+
+class sitepy:
+    def __init__(self, static_dir='static'):
+        self.routes = {}
+        self.middleware = [self.logger_middleware]
+        self.static_dir = static_dir
+
+    def route(self, path, methods=['GET']):
+        def wrapper(handler):
+            self.routes[(path, tuple(methods))] = handler
+            return handler
+        return wrapper
+
+    def use(self, middleware):
+        self.middleware.append(middleware)
+
+    def logger_middleware(self, environ):
+        print(f"Request received for {environ['PATH_INFO']}")
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+        method = environ['REQUEST_METHOD']
+        for middleware in self.middleware:
+            middleware(environ)
+        handler = None
+        for route, methods in self.routes.keys():
+            if path == route and method in methods:
+                handler = self.routes[(route, methods)]
+                break
+        if handler:
+            try:
+                request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+            except ValueError:
+                request_body_size = 0
+            request_body = environ['wsgi.input'].read(request_body_size)
+            params = parse_qs(request_body) if request_body else {}
+            handler_args = inspect.signature(handler).parameters
+            response_body = handler(params) if handler_args else handler()
+            status = '200 OK'
+            headers = [('Content-type', 'text/plain')]
+            start_response(status, headers)
+            return [response_body.encode()]
+        elif path.startswith('/' + self.static_dir):
+            try:
+                with open(path[1:], 'rb') as f:
+                    response_body = f.read()
+                status = '200 OK'
+                headers = [('Content-type', 'application/octet-stream')]
+                start_response(status, headers)
+                return [response_body]
+            except FileNotFoundError:
+                status = '404 NOT FOUND'
+                headers = [('Content-type', 'text/plain')]
+                start_response(status, headers)
+                return ['File not found'.encode()]
+        else:
+            status = '404 NOT FOUND'
+            headers = [('Content-type', 'text/plain')]
+            start_response(status, headers)
+            return ['Route not found'.encode()]
+
+    def run(self, host='localhost', port=8080):
+        try:
+            server = make_server(host, port, self)
+            print(f'Serving on {host}:{port}')
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print('\nServer shutting down...')
+            server.server_close()
+
+app = sitepy()
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
